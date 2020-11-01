f420797c0b9bbed5403a2698232061660d5b0e5f
ygybjc <ygybjc@qq.com>
2020-11-01 22:01:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;17.01' feeds.conf.default
 sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' feeds.conf.default
