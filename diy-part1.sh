56920dbe98919dcfcbe27a43d3d016e37bbc8b07
ygybjc <ygybjc@qq.com>
2021-08-07 06:41:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,4 @@
 # sed -i '$a https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-  sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
