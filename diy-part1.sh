3ce6476f40d42a5be2def2ec471729df5e07b202
ygybjc <ygybjc@qq.com>
2021-08-16 06:57:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,8 @@
 # sed -i '$a https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-package.git;master' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
- sed -i '$a src-git ygybjc https://github.com/ygybjc/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git lean https://github.com/jarviswwong/Lienol-lean-lede' feeds.conf.default
+sed -i 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git https://github.com/mingxiaoyu/lede-ssr-plus' feeds.conf.default
 # sed -i '$a src-git https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git https://github.com/pymumu/smartdns' feeds.conf.default
